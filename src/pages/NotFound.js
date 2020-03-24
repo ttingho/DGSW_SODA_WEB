81e5976798c6922de1595f3e00bb28aeb014438e
ohasesung1 <40197864+ohasesung1@users.noreply.github.com>
2020-03-24 16:21:47 +0900

    [ADD] notfound component

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import NotFoundTemplate from 'components/Notfound/NotFoundTemplate';
 
 const NotFound = () => {
   return (
     <div>
-      
+      <NotFoundTemplate>
+      </NotFoundTemplate>
     </div>
   );
 };
