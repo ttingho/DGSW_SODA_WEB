b76ab1fe360e17b67ffd8b5c3a499ac818f300a1
Myeongho <myeongho020812@gmail.com>
2020-04-22 18:41:44 +0900

    [FIX] Bamboo Write Page
    대숲 작성 페이지를 리디자인 했습니다. 이미지 수정 말고는 다 잘 작동합니다. 아마도

--- src/components/BambooWrite/BambooWriteTemplate/BambooWriteTemplate.js
+++ src/components/BambooWrite/BambooWriteTemplate/BambooWriteTemplate.js
@@ -29,87 +29,64 @@ const BambooWriteTemplate = ({
   handlePostRequest
 }) => {
   const customStyle = {
-    width: '145px',
-    height: '45px',
-    margin: 'auto 0 auto auto'
+    width: '18%',
+    height: '40%',
+    margin: 'auto 0 auto 3%'
   };
 
   const radioContents = <span className={cx('radioContents')}>
     실명 <span className={cx('radioContents-gray')}>(FaceBook 로그인 필요)</span>
   </span>; 
 
-  const { contents, setContents } = contentsObj;
-
   return (
     <div className={cx('BambooWriteTemplate')}>
-      <div className={cx('BambooWriteTemplate-leftPanel')}>
-        <textarea className={cx('BambooWriteTemplate-leftPanel-textarea')} placeholder={'여기에 당신의 대나무(이야기)를 적어주세요!'} value={contents} onChange={event => setContents(event.target.value)} />
-        <div className={cx('BambooWriteTemplate-leftPanel-image')}>
-          <div className={cx('BambooWriteTemplate-leftPanel-image-upload')}>
-            <label htmlFor={'image_upload'} className={cx('BambooWriteTemplate-leftPanel-image-upload-label')}>사진 업로드</label>
-            <input type={'file'} accept={'image/gif, image/jpeg, image/jpg, image/png'} id={'image_upload'} className={cx('BambooWriteTemplate-leftPanel-image-upload-input')} onChange={handleImageChange} multiple={'multiple'} />
-          </div>
-          <span className={cx('BambooWriteTemplate-leftPanel-image-detail')}>사진 파일 크기 : 4MB 이하</span>
-          <div className={cx('BambooWriteTemplate-leftPanel-image-exampleContents')} onClick={event => handleIsUpload(event)}>
-            <MdInsertPhoto className={cx('BambooWriteTemplate-leftPanel-image-exampleContents-icon')} onClick={event => handleIsUpload(event)} />
-            <span className={cx('BambooWriteTemplate-leftPanel-image-exampleContents-file')} onClick={event => handleIsUpload(event)}>
-              {imageContents}
-            </span>
-            <div className={cx('BambooWriteTemplate-leftPanel-image-exampleContents-fileList', { 'BambooWriteTemplate-hidden': !isUpload })}>
-              {
-                images.map((data, index) => {
-                  return <div key={index} className={cx('BambooWriteTemplate-leftPanel-image-exampleContents-fileList-item')}>
-                    <span className={cx('BambooWriteTemplate-leftPanel-image-exampleContents-fileList-item-name')}>{data.name}</span>
-                    <MdClose className={cx('BambooWriteTemplate-leftPanel-image-exampleContents-fileList-item-icon')} onClick={() => handleImageCancel(data.idx)} />
-                  </div>;
-                })
-              }
-            </div>
-          </div>
+      <div className={cx('BambooWriteTemplate-header')}>
+        <Radio
+          isRadio={isType}
+          radioType={'anonymous'}
+          color={'bamboo'} name={'bamboo-wrtie'}
+          id={'bamboo_radio_1'}
+          contents={'익명'}
+          customStyle={{ margin: 'auto 3% auto 0' }}
+          onChange={event => {
+            handleIsType(event);
+            if (accessToken) {
+              window.FB.logout();
+            }}}
+        />
+        <FacebookLogin
+          appId={FB_APP_ID_TEST}
+          autoLoad={false}
+          fields='name, email, picture'
+          callback={handleFaceBookLogin}
+          render={renderProps => (
+            <Radio
+              isRadio={isType}
+              radioType={'realname'}
+              color={'bamboo'}
+              name={'bamboo-wrtie'}
+              id={'bamboo_radio_2'}
+              contents={radioContents}
+              customStyle={{ margin: 'auto auto auto 0' }}
+              onChange={event => {
+                handleIsType(event);
+                renderProps.onClick();
+              }}
+            />
+          )}
+        />
+        <div className={cx('BambooWriteTemplate-header-upload')}>
+          <label htmlFor={'image_upload'} className={cx('BambooWriteTemplate-header-upload-label')}>사진 업로드</label>
+          <input type={'file'} accept={'image/gif, image/jpeg, image/jpg, image/png'} id={'image_upload'} className={cx('BambooWriteTemplate-header-upload-input')} onChange={handleImageChange} multiple={'multiple'} />
         </div>
+        <Button appearance={'secondary'} edgeType={'round'} customStyle={customStyle} handleFunction={handlePostRequest}>대나무 제보하기</Button>
       </div>
-      <div className={cx('BambooWriteTemplate-rightPanel')}>
-        <div className={cx('BambooWriteTemplate-rightPanel-top')}>
-          <Radio
-            isRadio={isType}
-            radioType={'anonymous'}
-            color={'bamboo'} name={'bamboo-wrtie'}
-            id={'bamboo_radio_1'}
-            contents={'익명'}
-            customStyle={{ margin: 'auto 3% auto 0' }}
-            onChange={event => {
-              handleIsType(event);
-              if (accessToken) {
-                window.FB.logout();
-              }}}
-          />
-          <FacebookLogin
-            appId={FB_APP_ID_TEST}
-            autoLoad={false}
-            fields='name, email, picture'
-            callback={handleFaceBookLogin}
-            render={renderProps => (
-              <Radio
-                isRadio={isType}
-                radioType={'realname'}
-                color={'bamboo'}
-                name={'bamboo-wrtie'}
-                id={'bamboo_radio_2'}
-                contents={radioContents}
-                customStyle={{ margin: 'auto auto auto 0' }}
-                onChange={event => {
-                  handleIsType(event);
-                  renderProps.onClick();
-                }}
-              />
-            )}
-          />
-          <Button appearance={'secondary'} edgeType={'round'} customStyle={customStyle} handleFunction={handlePostRequest}>대나무 제보하기</Button>
-        </div>
-        <div className={cx('BambooWriteTemplate-rightPanel-bottom')}>
-          <span className={cx('BambooWriteTemplate-rightPanel-bottom-title')}>게시물 미리 보기</span>
-          <ExampleCard profileSrc={profileSrc} name={name} contents={contents} images={imgBase64} />
-        </div>
+      <div className={cx('BambooWriteTemplate-contents')}>
+        <ExampleCard profileSrc={profileSrc} name={name} contentsObj={contentsObj} images={imgBase64} />
+      </div>
+      <div className={cx('BambooWriteTemplate-footer')}>
+        <span className={cx('BambooWriteTemplate-footer-title')}>참고</span>
+        <span className={cx('BambooWriteTemplate-footer-contents')}>최대 4MB 첨부 가능 (JPEG, JPG, PNG 파일 지원)</span>
       </div>
     </div>
   );
--- src/components/BambooWrite/ExampleCard/ExampleCard.js
+++ src/components/BambooWrite/ExampleCard/ExampleCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TextareaAutosize from 'react-textarea-autosize';
 import { FaFacebookF } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
@@ -8,7 +9,9 @@ import Pagination from 'components/Common/Pagination';
 
 const cx = classNames.bind(style);
 
-const ExampleCard = ({ profileSrc, name, contents, images }) => {
+const ExampleCard = ({ profileSrc, name, contentsObj, images }) => {
+  const { contents, setContents } = contentsObj;
+
   return (
     <div className={cx('ExampleCard')}>
       <div className={cx('ExampleCard-header')}>
@@ -27,18 +30,15 @@ const ExampleCard = ({ profileSrc, name, contents, images }) => {
           <FaFacebookF className={cx('ExampleCard-header-facebook-icon')} />
         </div>
       </div>
-      <div className={cx('ExampleCard-contents-images')}>
+      <div className={cx('ExampleCard-images')}>
         <Pagination images={images} />
       </div>
-      <div className={cx('ExampleCard-contents')}>
-        <p className={cx('ExampleCard-contents-text')}>
-          {
-            contents.length === 0 ?
-              '옆에 대나무(이야기)를 적으면 미리 볼 수 있어요!' :
-              contents
-          }
-        </p>
-      </div>
+      <TextareaAutosize
+        value={contents}
+        onChange={event => setContents(event.target.value)}
+        className={cx('ExampleCard-contents')}
+        placeholder={'여기에 당신의 대나무(이야기)를 적어주세요!'}
+      />
     </div>
   );
 };
@@ -46,7 +46,7 @@ const ExampleCard = ({ profileSrc, name, contents, images }) => {
 ExampleCard.propTypes = {
   profileSrc: PropTypes.string,
   name: PropTypes.string,
-  contents: PropTypes.string,
+  contentsObj: PropTypes.object,
   images: PropTypes.array
 };
 
